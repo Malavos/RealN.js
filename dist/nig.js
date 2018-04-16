0f729636417330a4b6452555cc5f41a7981fdc7b
Malavos <malavos@hotmail.com>
2018-04-16 15:00:42 -0300

    Updated nig.js
    Added our function to search for nodes by attribute.

--- dist/nig.js
+++ dist/nig.js
@@ -1,7 +1,21 @@
+niggaBind = function(event){
+    var elem =document.getElementsByClassName(event.currentTarget.className);
+    for(var key in elem){
+        elem[key].value=event.currentTarget.value;
+    }
+};
 
-niggaBind=function(event){
-var elem =document.getElementsByClassName(event.currentTarget.className);
-for(var key in elem){
-    elem[key].value=event.currentTarget.value;
-}
+//This was taken from https://stackoverflow.com/a/15342661/2890522. Kudos to yckart.
+//a stands for Attribute. C stands 
+function getElementsByAttribute(a, c) {
+  var nodeList = (context || document).getElementsByTagName('*');
+  var nodeArray = [];
+  var iterator = 0;
+  var node = null;
+
+  while (node = nodeList[iterator++]) {
+    if (node.hasAttribute(attribute)) nodeArray.push(node);
+  }
+
+  return nodeArray;
 }
